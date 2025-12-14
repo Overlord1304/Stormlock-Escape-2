3e7e535722f7e481afbb6c2c4e2e652dae6bb57c
unknown <overlordxanime0@gmail.com>
2025-12-14 12:05:25 +0400

    player attacks work now!

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,8 +3,12 @@ extends CharacterBody2D
 var speed = 60
 var player_chase = false
 var player = null
+var health = 80
+var player_inattack_zone = false
+var can_take_damage = true
 
 func _physics_process(delta) -> void:
+	deal_with_damage()
 	if player_chase and player:
 		var to_player = player.position - position
 		var distance = to_player.length()
@@ -30,3 +34,29 @@ func _on_area_2d_body_exited(body) -> void:
 	if body.is_in_group("player"):
 		player = null
 		player_chase = false
+
+func enemy():
+	pass
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = true
+
+
+func _on_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = false
+		
+func deal_with_damage():
+	if player_inattack_zone and Global.player_current_attack == true:
+		if can_take_damage == true:
+			health = health - 20
+			$take_damage_cooldown.start()
+			can_take_damage = false
+			if health <= 0:
+				self.queue_free()
+
+
+func _on_take_damage_cooldown_timeout() -> void:
+	can_take_damage = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,12 @@
 extends CharacterBody2D
 
+var enemy_inattack_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var player_alive = true
+
+var attack_ip = false
+
 @export var speed = 100
 
 var direction = Vector2.ZERO
@@ -8,6 +15,11 @@ var last_dir = "down"
 
 
 func _physics_process(delta):
+	attack()
+	enemy_attack()
+	if health <= 0:
+		player_alive = false
+		health = 0
 	direction = Vector2.ZERO
 	direction.x = Input.get_axis("ui_left","ui_right")
 	direction.y = Input.get_axis("ui_up","ui_down")
@@ -19,6 +31,8 @@ func _physics_process(delta):
 	_update_anim()
 
 func _update_anim():
+	if attack_ip:
+		return
 	var anim = ""
 	
 	if direction != Vector2.ZERO:
@@ -28,6 +42,54 @@ func _update_anim():
 			last_dir = "down" if direction.y > 0 else "up"
 		anim = "walk_" + last_dir
 	else:
-		anim = "idle_" + last_dir
+		if attack_ip == false:
+			anim = "idle_" + last_dir
 	
 	$AnimatedSprite2D.play(anim) 
+
+func player():
+	pass
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_inattack_range = true
+		
+
+
+func _on_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_inattack_range = false
+		
+func enemy_attack():
+	if enemy_inattack_range and enemy_attack_cooldown == true:
+		health = health - 10
+		print(health)
+		enemy_attack_cooldown = false
+		$attack_cooldown.start()
+
+
+func _on_timer_timeout() -> void:
+	enemy_attack_cooldown = true
+
+func attack():
+	if Input.is_action_just_pressed("attack") and attack_ip == false:
+		attack_ip = true
+		Global.player_current_attack = true
+		
+		$deal_attack_timer.start()
+		
+		match last_dir:
+			"right":
+				$AnimatedSprite2D.play("attack_right")
+			"left":
+				$AnimatedSprite2D.play("attack_left")
+			"down":
+				$AnimatedSprite2D.play("attack_down")
+			"up":
+				$AnimatedSprite2D.play("attack_up")
+
+
+func _on_deal_attack_timer_timeout() -> void:
+	$deal_attack_timer.stop()
+	Global.player_current_attack = false
+	attack_ip = false
